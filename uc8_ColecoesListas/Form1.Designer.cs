30291ed01ab2e7aa423a0b74245013ad5b6a5a38
ArthurLovat <articfin.eco@gmail.com>
2024-10-18 20:56:44 -0300

    2.0v

--- uc8_ColecoesListas/Form1.Designer.cs
+++ uc8_ColecoesListas/Form1.Designer.cs
@@ -32,12 +32,18 @@
             txtNome = new TextBox();
             btnAdicionar = new Button();
             txtLista = new TextBox();
+            btnExcluir = new Button();
+            label2 = new Label();
+            txtNovoNome = new TextBox();
+            brnAlterar = new Button();
+            btnCrescente = new Button();
+            btnDecrescente = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(11, 41);
+            label1.Location = new Point(44, 40);
             label1.Name = "label1";
             label1.Size = new Size(40, 15);
             label1.TabIndex = 0;
@@ -45,16 +51,16 @@
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(55, 38);
+            txtNome.Location = new Point(94, 37);
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(100, 23);
             txtNome.TabIndex = 1;
             // 
             // btnAdicionar
             // 
-            btnAdicionar.Location = new Point(80, 67);
+            btnAdicionar.Location = new Point(94, 83);
             btnAdicionar.Name = "btnAdicionar";
-            btnAdicionar.Size = new Size(75, 23);
+            btnAdicionar.Size = new Size(100, 23);
             btnAdicionar.TabIndex = 2;
             btnAdicionar.Text = "Adicionar";
             btnAdicionar.UseVisualStyleBackColor = true;
@@ -62,18 +68,80 @@
             // 
             // txtLista
             // 
-            txtLista.Location = new Point(55, 126);
+            txtLista.Location = new Point(266, 41);
             txtLista.Multiline = true;
             txtLista.Name = "txtLista";
             txtLista.ScrollBars = ScrollBars.Horizontal;
             txtLista.Size = new Size(100, 312);
             txtLista.TabIndex = 3;
             // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(94, 112);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(100, 23);
+            btnExcluir.TabIndex = 4;
+            btnExcluir.Text = "Excluir";
+            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Novo Nome";
+            // 
+            // txtNovoNome
+            // 
+            txtNovoNome.Location = new Point(94, 180);
+            txtNovoNome.Name = "txtNovoNome";
+            txtNovoNome.Size = new Size(100, 23);
+            txtNovoNome.TabIndex = 6;
+            // 
+            // brnAlterar
+            // 
+            brnAlterar.Location = new Point(94, 209);
+            brnAlterar.Name = "brnAlterar";
+            brnAlterar.Size = new Size(100, 23);
+            brnAlterar.TabIndex = 7;
+            brnAlterar.Text = "Alterar";
+            brnAlterar.UseVisualStyleBackColor = true;
+            brnAlterar.Click += brnAlterar_Click;
+            // 
+            // btnCrescente
+            // 
+            btnCrescente.Location = new Point(94, 279);
+            btnCrescente.Name = "btnCrescente";
+            btnCrescente.Size = new Size(130, 23);
+            btnCrescente.TabIndex = 8;
+            btnCrescente.Text = "Order Crescente";
+            btnCrescente.UseVisualStyleBackColor = true;
+            btnCrescente.Click += btnCrescente_Click;
+            // 
+            // btnDecrescente
+            // 
+            btnDecrescente.Location = new Point(94, 330);
+            btnDecrescente.Name = "btnDecrescente";
+            btnDecrescente.Size = new Size(130, 23);
+            btnDecrescente.TabIndex = 9;
+            btnDecrescente.Text = "Ordem Decrescente";
+            btnDecrescente.UseVisualStyleBackColor = true;
+            btnDecrescente.Click += btnDecrescente_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(416, 450);
+            Controls.Add(btnDecrescente);
+            Controls.Add(btnCrescente);
+            Controls.Add(brnAlterar);
+            Controls.Add(txtNovoNome);
+            Controls.Add(label2);
+            Controls.Add(btnExcluir);
             Controls.Add(txtLista);
             Controls.Add(btnAdicionar);
             Controls.Add(txtNome);
@@ -90,5 +158,11 @@
         private TextBox txtNome;
         private Button btnAdicionar;
         private TextBox txtLista;
+        private Button btnExcluir;
+        private Label label2;
+        private TextBox txtNovoNome;
+        private Button brnAlterar;
+        private Button btnCrescente;
+        private Button btnDecrescente;
     }
 }
--- uc8_ColecoesListas/Form1.cs
+++ uc8_ColecoesListas/Form1.cs
@@ -14,13 +14,50 @@ namespace uc8_ColecoesListas
         private void btnAdicionar_Click(object sender, EventArgs e)
         {
             nomes.Add(txtNome.Text);
+            AtualizarNomes();
+        }
 
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            nomes.Remove(txtNome.Text);
+            AtualizarNomes();
+
+        }
+        private void AtualizarNomes()
+        {
             txtLista.Clear();
-
-            for(int i = 0; i < nomes.Count; i++)
+            for (int i = 0; i < nomes.Count; i++)
             {
                 txtLista.AppendText(nomes[i] + Environment.NewLine);
             }
+
+            //foreach (string nome in nomes) outra forma de resolver
+            //{
+            //    txtLista.AppendText(nome + Environment.NewLine);
+            //}
+        }
+
+        private void brnAlterar_Click(object sender, EventArgs e)
+        {
+            int idx = nomes.IndexOf(txtNome.Text);
+
+            if (idx > -1)
+            {
+                nomes[idx] = txtNovoNome.Text;
+            }
+            AtualizarNomes();
+        }
+
+        private void btnCrescente_Click(object sender, EventArgs e)
+        {
+            nomes.Sort();
+            AtualizarNomes();
+        }
+
+        private void btnDecrescente_Click(object sender, EventArgs e)
+        {
+            nomes.Reverse();
+            AtualizarNomes();
         }
     }
 }
